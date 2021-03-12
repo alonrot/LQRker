f9c726e9eb016081cf73fd362af52c4ba14b057f
Alonso Marco-Valle <marcovalle.a@gmail.com>
2021-03-12 01:15:40 +0100

    Minor

--- lqrker/experiments/cluster/launch.sh
+++ lqrker/experiments/cluster/launch.sh
@@ -7,4 +7,10 @@ echo "(2) repetition number:       " $2
 
 # export LD_LIBRARY_PATH=/lustre/home/amarcovalle/.mujoco/mujoco200/bin:/home/amarcovalle/.mujoco/mujoco200/bin:$LD_LIBRARY_PATH
 
-/home/amarcovalle/.anaconda3/envs/classireg/bin/python run_experiments.py cluster.which_model=$1 cluster.rep_nr=$2
+/home/amarcovalle/.anaconda3/envs/lqrker/bin/python run_experiments.py cluster.which_model=$1 cluster.rep_nr=$2
+
+
+# condor_submit_bid 500 cluster/launch_GPFLOW.sub
+# condor_q
+# condor_rm
+# alias check="condor_q amarcovalle"
