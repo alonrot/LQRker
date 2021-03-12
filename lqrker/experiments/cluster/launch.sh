63cd45791144f07b23dd965df6f70533b6068631
Alonso Marco-Valle <marcovalle.a@gmail.com>
2021-03-12 01:18:32 +0100

    Minrot

--- lqrker/experiments/cluster/launch.sh
+++ lqrker/experiments/cluster/launch.sh
@@ -7,7 +7,7 @@ echo "(2) repetition number:       " $2
 
 # export LD_LIBRARY_PATH=/lustre/home/amarcovalle/.mujoco/mujoco200/bin:/home/amarcovalle/.mujoco/mujoco200/bin:$LD_LIBRARY_PATH
 
-/home/amarcovalle/.anaconda3/envs/lqrker/bin/python run_experiments.py cluster.which_model=$1 cluster.rep_nr=$2
+/home/amarcovalle/.anaconda3/envs/lqrker/bin/python validate_model_cluster.py cluster.which_model=$1 cluster.rep_nr=$2
 
 
 # condor_submit_bid 500 cluster/launch_GPFLOW.sub
