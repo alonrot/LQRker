2c5d35eb9ea909f6f47f6d5bbbda4ba3d40da875
Alonso Marco-Valle <marcovalle.a@gmail.com>
2021-03-21 19:54:05 +0100

    WIP

--- lqrker/tests/test_lqr_kernel_logGP.py
+++ lqrker/tests/test_lqr_kernel_logGP.py
@@ -14,6 +14,89 @@ from lqrker.models.lqr_kernel_trans_gpflow import LQRkernelTransformed, LQRMeanT
 from lqrker.utils.parsing import get_logger
 logger = get_logger(__name__)
 
+
+def model_LQRcost_as_GP(cfg,X,Y,A,B,xpred):
+
+	# Split dataset:
+	Xtrain, Ytrain, Xtest, Ytest = split_dataset(X,Y,
+												perc_training=cfg.validation.perc_training,
+												Ncut=cfg.validation.Ncut)
+
+	if isinstance(cfg.dataset.dim,str):
+		dim = eval(cfg.dataset.dim)
+	else:
+		dim = cfg.dataset.dim
+
+	lqr_ker = LQRkernel(cfg=cfg.RRTPLQRfeatures,dim=dim,A_samples=A,B_samples=B)
+	lqr_mean = LQRMean(cfg=cfg.RRTPLQRfeatures,dim=dim,A_samples=A,B_samples=B)
+
+	XX = tf.cast(Xtrain,dtype=tf.float64)
+	YY = tf.cast(tf.reshape(Ytrain,(-1,1)),dtype=tf.float64)
+	mod = gpflow.models.GPR(data=(XX,YY), kernel=lqr_ker, mean_function=lqr_mean)
+	sigma_n = cfg.RRTPLQRfeatures.hyperpars.sigma_n.init
+	mod.likelihood.variance.assign(sigma_n**2)
+	xxpred = tf.cast(xpred,dtype=tf.float64)
+	# opt = gpflow.optimizers.Scipy()
+	# opt_logs = opt.minimize(mod.training_loss, mod.trainable_variables, options=dict(maxiter=300))
+	gpflow.utilities.print_summary(mod)
+	mean_pred_gpflow, var_pred_gpflow = mod.predict_f(xxpred)
+
+	mean_vec = mean_pred_gpflow
+	
+	std_pred_gpflow = np.sqrt(var_pred_gpflow)
+	fpred_quan_minus = mean_pred_gpflow - 2.*std_pred_gpflow
+	fpred_quan_plus = mean_pred_gpflow + 2.*std_pred_gpflow
+
+	return mean_vec, fpred_quan_minus, fpred_quan_plus, Xtrain, Ytrain
+
+def model_LQRcost_as_logGP(cfg,X,Y,A,B,xpred):
+
+	Y = tf.math.log(Y)
+
+	# Split dataset:
+	Xtrain, Ytrain, Xtest, Ytest = split_dataset(X,Y,
+												perc_training=cfg.validation.perc_training,
+												Ncut=cfg.validation.Ncut)
+
+	if isinstance(cfg.dataset.dim,str):
+		dim = eval(cfg.dataset.dim)
+	else:
+		dim = cfg.dataset.dim
+
+	lqr_ker = LQRkernelTransformed(cfg=cfg.RRTPLQRfeatures,dim=dim,A_samples=A,B_samples=B)
+	lqr_mean = LQRMeanTransformed(cfg=cfg.RRTPLQRfeatures,dim=dim,A_samples=A,B_samples=B)
+
+	XX = tf.cast(Xtrain,dtype=tf.float64)
+	YY = tf.cast(tf.reshape(Ytrain,(-1,1)),dtype=tf.float64)
+	mod = gpflow.models.GPR(data=(XX,YY), kernel=lqr_ker, mean_function=lqr_mean)
+	sigma_n = cfg.RRTPLQRfeatures.hyperpars.sigma_n.init
+	mod.likelihood.variance.assign(sigma_n**2)
+	xxpred = tf.cast(xpred,dtype=tf.float64)
+	# opt = gpflow.optimizers.Scipy()
+	# opt_logs = opt.minimize(mod.training_loss, mod.trainable_variables, options=dict(maxiter=300))
+	gpflow.utilities.print_summary(mod)
+	mean_pred_gpflow, var_pred_gpflow = mod.predict_f(xxpred)
+
+	# mean_vec = tf.exp( mean_pred_gpflow + 0.5 * var_pred_gpflow ) # Mean
+	# mean_vec = tf.exp( mean_pred_gpflow - var_pred_gpflow ) # Mode
+	mean_vec = tf.exp( mean_pred_gpflow ) # Median
+
+	# The noise sigma_n plays an important role
+
+	fpred_quan_plus = tf.exp( mean_pred_gpflow  + tf.sqrt(2.0*var_pred_gpflow) * tf.cast(tf.math.erfinv(2.*0.95 - 1.),dtype=tf.float64) )
+	fpred_quan_minus = tf.exp( mean_pred_gpflow  + tf.sqrt(2.0*var_pred_gpflow) * tf.cast(tf.math.erfinv(2.*0.05 - 1.),dtype=tf.float64) )
+
+	# pdb.set_trace()
+	Ytrain = tf.exp(Ytrain)
+
+	# mean_vec = mean_pred_gpflow
+	
+	# std_pred_gpflow = np.sqrt(var_pred_gpflow)
+	# fpred_quan_plus = mean_pred_gpflow + 2.*std_pred_gpflow
+	# fpred_quan_minus = mean_pred_gpflow - 2.*std_pred_gpflow
+
+	return mean_vec, fpred_quan_minus, fpred_quan_plus, Xtrain, Ytrain
+
 @hydra.main(config_path="../experiments/",config_name="config.yaml")
 def main(cfg: dict) -> None:
 	"""
@@ -29,89 +112,38 @@ def main(cfg: dict) -> None:
 	np.random.seed(my_seed)
 	tf.random.set_seed(my_seed)
 
-	activate_log_process = False
-	# activate_log_process = True
-
-	if isinstance(cfg.dataset.dim,str):
-		dim = eval(cfg.dataset.dim)
-	else:
-		dim = cfg.dataset.dim
-
-	X,Y,A,B = generate_dataset(cfg)
-
-	if activate_log_process:
-		Y = tf.math.log(Y)
-
-	# Split dataset:
-	Xtrain, Ytrain, Xtest, Ytest = split_dataset(X,Y,
-												perc_training=cfg.validation.perc_training,
-												Ncut=cfg.validation.Ncut)
+	# activate_log_process = False
+	activate_log_process = True
 
 	xlim = eval(cfg.dataset.xlims)
 
 	Npred = 60
 	xpred = 10**tf.reshape(tf.linspace(xlim[0],xlim[1],Npred),(-1,1))
 
-	# Regression with gpflow:
-	if activate_log_process:
-		lqr_ker = LQRkernelTransformed(cfg=cfg.RRTPLQRfeatures,dim=dim,A_samples=A,B_samples=B)
-		lqr_mean = LQRMeanTransformed(cfg=cfg.RRTPLQRfeatures,dim=dim,A_samples=A,B_samples=B)
-	else:
-		lqr_ker = LQRkernel(cfg=cfg.RRTPLQRfeatures,dim=dim,A_samples=A,B_samples=B)
-		lqr_mean = LQRMean(cfg=cfg.RRTPLQRfeatures,dim=dim,A_samples=A,B_samples=B)
+	X,Y,A,B = generate_dataset(cfg)
 
-	# pdb.set_trace()
-
-	XX = tf.cast(Xtrain,dtype=tf.float64)
-	YY = tf.cast(tf.reshape(Ytrain,(-1,1)),dtype=tf.float64)
-	mod = gpflow.models.GPR(data=(XX,YY), kernel=lqr_ker, mean_function=lqr_mean)
-	# sigma_n = cfg.RRTPLQRfeatures.hyperpars.sigma_n.init
-	sigma_n = 1.0
-	mod.likelihood.variance.assign(sigma_n**2)
-	xxpred = tf.cast(xpred,dtype=tf.float64)
-	# opt = gpflow.optimizers.Scipy()
-	# opt_logs = opt.minimize(mod.training_loss, mod.trainable_variables, options=dict(maxiter=300))
-	gpflow.utilities.print_summary(mod)
-	mean_pred_gpflow, var_pred_gpflow = mod.predict_f(xxpred)
-
-	if activate_log_process:
-
-		# mean_vec = tf.exp( mean_pred_gpflow + 0.5 * var_pred_gpflow ) # Mean
-		# mean_vec = tf.exp( mean_pred_gpflow - var_pred_gpflow ) # Mode
-		mean_vec = tf.exp( mean_pred_gpflow ) # Median
-
-		# The noise sigma_n plays an important role
-
-		fpred_quan_plus = tf.exp( mean_pred_gpflow  + tf.sqrt(2.0*var_pred_gpflow) * tf.cast(tf.math.erfinv(2.*0.95 - 1.),dtype=tf.float64) )
-		fpred_quan_minus = tf.exp( mean_pred_gpflow  + tf.sqrt(2.0*var_pred_gpflow) * tf.cast(tf.math.erfinv(2.*0.05 - 1.),dtype=tf.float64) )
-
-		# pdb.set_trace()
-		Ytrain = tf.exp(Ytrain)
-
-		# mean_vec = mean_pred_gpflow
-		
-		# std_pred_gpflow = np.sqrt(var_pred_gpflow)
-		# fpred_quan_plus = mean_pred_gpflow + 2.*std_pred_gpflow
-		# fpred_quan_minus = mean_pred_gpflow - 2.*std_pred_gpflow
-
-
-	else:
-
-		mean_vec = mean_pred_gpflow
-		
-		std_pred_gpflow = np.sqrt(var_pred_gpflow)
-		fpred_quan_plus = mean_pred_gpflow + 2.*std_pred_gpflow
-		fpred_quan_minus = mean_pred_gpflow - 2.*std_pred_gpflow
+	mean_vec_logGP, fpred_quan_minus_logGP, fpred_quan_plus_logGP, Xtrain, Ytrain_logGP = model_LQRcost_as_logGP(cfg,X,Y,A,B,xpred)
+	mean_vec, fpred_quan_minus, fpred_quan_plus, Xtrain, Ytrain = model_LQRcost_as_GP(cfg,X,Y,A,B,xpred)
 
 
 	hdl_fig, hdl_splots = plt.subplots(2,1,figsize=(14,10),sharex=True)
-	hdl_splots[0].plot(xxpred,mean_vec)
-	hdl_splots[0].fill(tf.concat([xxpred, xxpred[::-1]],axis=0),tf.concat([fpred_quan_minus,(fpred_quan_plus)[::-1]],axis=0),\
+	hdl_splots[0].plot(xpred,mean_vec_logGP)
+	hdl_splots[0].fill(tf.concat([xpred, xpred[::-1]],axis=0),tf.concat([fpred_quan_minus_logGP,(fpred_quan_plus_logGP)[::-1]],axis=0),\
 		alpha=.2, fc="blue", ec='None')
+	hdl_splots[0].set_title("logGP with LQR kernel. We do GP regression on f")
 	hdl_splots[0].set_xlabel("x")
-	hdl_splots[0].set_xlim(xxpred[0,0],xxpred[-1,0])
-	hdl_splots[0].plot(Xtrain,Ytrain,marker="o",color="black",linestyle="None")
+	hdl_splots[0].set_xlim(xpred[0,0],xpred[-1,0])
+	hdl_splots[0].plot(Xtrain,Ytrain_logGP,marker="o",color="black",linestyle="None")
 
+
+	hdl_splots[1].plot(xpred,mean_vec)
+	hdl_splots[1].fill(tf.concat([xpred, xpred[::-1]],axis=0),tf.concat([fpred_quan_minus,(fpred_quan_plus)[::-1]],axis=0),\
+		alpha=.2, fc="blue", ec='None')
+	hdl_splots[1].set_title("Standard GP with LQR kernel. We do GP regression on J")
+	hdl_splots[1].set_xlabel("x")
+	hdl_splots[1].set_xlim(xpred[0,0],xpred[-1,0])
+	hdl_splots[1].plot(Xtrain,Ytrain,marker="o",color="black",linestyle="None")
+	hdl_splots[1].set_xlabel("x")
 
 	plt.show(block=True)
 
